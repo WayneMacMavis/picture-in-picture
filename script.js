969af2cfd9ed3fc7816a484049b9e82e4a1e3ef2
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-08-09 11:36:50 +0200

    pip update

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const video = document.getElementById('video');
-const togglePipButton = document.getElementById('togglePipButton');
+const videoElement = document.getElementById('video');
+const togglePipButton = document.getElementById('button');
 
- // Hide button if Picture-in-Picture is not supported or disabled.
-togglePipButton.hidden = !document.pictureInPictureEnabled ||
-video.disablePictureInPicture;
-
-togglePipButton.addEventListener('click', function() {
-    // If there is no element in Picture-in-Picture yet, let’s request
-    // Picture-in-Picture for the video, otherwise leave it.
-    if (!document.pictureInPictureElement) {
-    video.requestPictureInPicture()
-    .catch(error => {
-    // Video failed to enter Picture-in-Picture mode.
-    });
-    } else {
-    document.exitPictureInPicture()
-    .catch(error => {
-        // Video failed to leave Picture-in-Picture mode.
-    });
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch error here
+        console.log('whoops, error here', error);
     }
-});
+}
+
+// On load
+selectMediaStream();
