e39bf02f0cf07af87ebc6b47f3e9544a2b0ad0c3
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-08-23 15:48:28 +0200

    Web Cam

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const videoElement = document.getElementById('video');
 const togglePipButton = document.getElementById('button');
+const webCam = document.getElementById('web-cam');
+const stopButton = document.getElementById('stop');
+const video = document.createElement('video');
 
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
@@ -15,6 +18,15 @@ async function selectMediaStream() {
     }
 }
 
+webCam.addEventListener('click', async () => {
+    video.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.play();
+    video.addEventListener('loadedmetadata', () => {
+    video.requestPictureInPicture()
+    .catch(console.error)
+      });
+    });
+
 togglePipButton.addEventListener('click', async () => {
     // Disable button
     togglePipButton.disabled = true;
@@ -23,6 +35,5 @@ togglePipButton.addEventListener('click', async () => {
     // Reset button
     togglePipButton.disabled = false;
 });
-
 // On load
 selectMediaStream();
